39ff657a7a3b3b1b1aeba7fa3eb9442f1f9cb0e2
kromero16 <kekvn13@live.com>
2016-12-30 12:29:35 -0800

    Updated Circular Linked List

--- CircularLinkedList/main.cpp
+++ CircularLinkedList/main.cpp
@@ -4,17 +4,23 @@
  * Purpose: Main Implementation of a Circular Linked List
  * Created on December 30, 2016, 9:04 AM
  */
+
 //System Libraries
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
 //User Libraries
+#include "CircularLinkedList.h"
 
 //Function Prototypes
 
 //Begin Execution
 int main(int argc, char** argv) {
+    CircularLinkedList<int> list;
+    
+    list.push_back(15);
+    list.printList();
 
     
     //Exit Program
