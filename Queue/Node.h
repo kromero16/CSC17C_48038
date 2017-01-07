5fefa124ecc2587ced7e74eb7797c1ba632efa17
kromero16 <kevkvn13@live.com>
2017-01-07 09:06:19 -0800

    Started on Queue Using Linked List

---
+++ Queue/Node.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Node.h
+ * Author: kevin
+ *
+ * Created on January 7, 2017, 9:03 AM
+ */
+
+#ifndef NODE_H
+#define NODE_H
+
+
+
+#endif /* NODE_H */
+
---
+++ Queue/Queue.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Queue.h
+ * Author: kevin
+ *
+ * Created on January 7, 2017, 9:04 AM
+ */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+
+
+#endif /* QUEUE_H */
+
